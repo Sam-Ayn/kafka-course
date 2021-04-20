2d7e7a8d30ce4eb1d52415386ec270b501c68b9d
Зотин Дмитрий Сергеевич <DSZotin@sberbank.ru>
2021-04-20 13:01:26 +0300

    Initial commit

---
+++ src/main/java/ru/kafkacourse/lessons/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,31 @@
+package ru.kafkacourse.lessons.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<String, String>("first_topic", "hello world");
+
+        producer.send(producerRecord);
+        producer.flush();
+        producer.close();
+
+        System.out.println("HelloWorld");
+    }
+}
